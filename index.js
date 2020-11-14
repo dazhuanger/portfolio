0f9d5ea334544d06fbf9fd465f3f8e552383ecdb
Jianchen Zhu <49572504+dazhuanger@users.noreply.github.com>
2020-11-13 22:20:45 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   /* Every time the window is scrolled ... */
-  $(window).scroll(function() {
-
+  $(window).scroll(function () {
     /* Check the location of each desired element */
-    $('.hideme').each(function(i) {
-
+    $(".hideme").each(function (i) {
       var bottom_of_object = $(this).offset().top + 300;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-      /* If the object is completely visible in the window, fade it it */
+      /* If the object is completely visible in the window, fade it in */
       if (bottom_of_window > bottom_of_object) {
-
-        $(this).animate({
-          'opacity': '1'
-        }, 1000);
+        $(this).animate(
+          {
+            opacity: "1",
+          },
+          1000
+        );
 
         $(".scroll").removeClass("floating");
       }
-
     });
-
   });
 
+  // Horizontal slider
+  const slider = document.querySelector(".more-projects");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+  });
 });
